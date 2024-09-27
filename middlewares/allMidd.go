6225065ecdb86abd6b829b745f5465fd77bdce8b
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-27 16:45:42 +0800

    add: 添加跨域请求配置

--- middlewares/allMidd.go
+++ middlewares/allMidd.go
@@ -1,9 +1,15 @@
 package middlewares
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitMiddleware(ginRoute *gin.Engine) {
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"*"}
+	corsConfig.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "PATCH"}
+	corsConfig.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	ginRoute.Use(cors.New(corsConfig))
 	ginRoute.Use(RecordLogs())
 }
