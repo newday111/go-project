f98dee7c8cdcabfe96cd92d8df7be10c1e9488f9
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-27 16:46:35 +0800

    fix: 修改用户访问记录日志

--- middlewares/recordLogs.go
+++ middlewares/recordLogs.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"goPro4/utils"
 )
@@ -8,6 +9,8 @@ import (
 func RecordLogs() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//	这里需要记录请求路径 和 访问用户
-		utils.Info(c.Request.URL)
+		c.Next()
+		userResponseInfo := fmt.Sprintf("user_account: %s | request_url: %s | response_status: %d", "xiaoming", c.Request.URL, c.Writer.Status())
+		utils.Info(userResponseInfo)
 	}
 }
