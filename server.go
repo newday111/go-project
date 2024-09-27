d6e0069ddd1b8a1436f5eb606d81703f032fced3
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-27 10:57:33 +0800

    add: 添加日志初始化

--- server.go
+++ server.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"goPro4/db"
 	"goPro4/routes/allRoute"
+	"goPro4/utils"
 )
 
 func main() {
 	db.GetMysqlConnection()
+	utils.InitLogger()
 	ginRouter := gin.Default()
 	allRoute.InitRouter(ginRouter)
 	ginRouter.Run(":8080")
