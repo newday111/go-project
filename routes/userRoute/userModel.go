848c59251015849281521dc93e9aedb074238fa4
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-27 17:56:39 +0800

    delete: 删除查询写入日志文件

--- routes/userRoute/userModel.go
+++ routes/userRoute/userModel.go
@@ -2,7 +2,6 @@ package userRoute
 
 import (
 	"goPro4/db"
-	"goPro4/utils"
 )
 
 const loginUserTableName = "login_users"
@@ -16,6 +15,5 @@ type loginUser struct {
 func (userInfo *loginUser) getUserInfoToName(username string) *loginUser {
 	//	指定查询的表名
 	db.DB.Table(loginUserTableName).Select([]string{"name", "token", "is_valid"}).Where("name = ?", username).Find(userInfo)
-	utils.Info("userInfo query username -> " + userInfo.UserName)
 	return userInfo
 }
