45abb98dd85787827e1200f7b6f824bf91de154c
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-27 11:37:07 +0800

    add: 查询用户记录日志

--- routes/userRoute/userModel.go
+++ routes/userRoute/userModel.go
@@ -2,6 +2,7 @@ package userRoute
 
 import (
 	"goPro4/db"
+	"goPro4/utils"
 )
 
 type loginUser struct {
@@ -12,6 +13,7 @@ type loginUser struct {
 
 func (userInfo *loginUser) getUserInfoToName(username string) *loginUser {
 	//	指定查询的表名
-	db.DB.Table("login_users").Select([]string{"name", "token", "is_valid"}).Where("name = ?", username).Find(&userInfo)
+	db.DB.Table("login_users").Select([]string{"name", "token", "is_valid"}).Where("name = ?", username).Find(userInfo)
+	utils.Info("userInfo query username -> " + userInfo.UserName)
 	return userInfo
 }
