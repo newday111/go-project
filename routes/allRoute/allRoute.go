d1b18cb953bcd7a3df84b340b9f26921a5ffd617
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-30 22:52:09 +0800

    add:添加专辑初始化路由

--- routes/allRoute/allRoute.go
+++ routes/allRoute/allRoute.go
@@ -2,10 +2,12 @@ package allRoute
 
 import (
 	"github.com/gin-gonic/gin"
+	"goPro4/routes/albumRoute"
 	"goPro4/routes/userRoute"
 )
 
 func InitRouter(router *gin.Engine) {
 	allRouter := router.Group("/v1")
 	userRoute.InitUserRouter(allRouter)
+	albumRoute.InitAlbumRouter(allRouter)
 }
