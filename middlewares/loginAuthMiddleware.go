ab5faa0e460f02c91fced67d9e96a9a38000454f
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-30 09:01:58 +0800

    添加登录之后用户校验

---
+++ middlewares/loginAuthMiddleware.go
@@ -0,0 +1,33 @@
+package middlewares
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"net/http"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		if tokenString == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Missing token"})
+			return
+		}
+
+		token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %s", token.Header["alg"])
+			}
+			jwtKey := []byte(viper.GetString("login_auth.secret_key"))
+			return jwtKey, nil
+		})
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("username", claims["username"])
+		}
+		c.Next()
+	}
+}
