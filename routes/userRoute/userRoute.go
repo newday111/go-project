b131f36c86179221fa24b30f0c495e4934ccf4c8
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-30 08:47:19 +0800

    add: 添加路由权限认证

--- routes/userRoute/userRoute.go
+++ routes/userRoute/userRoute.go
@@ -1,11 +1,14 @@
 package userRoute
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"goPro4/middlewares"
+)
 
 func InitUserRouter(Router *gin.RouterGroup) {
 	userRoute := Router.Group("/user")
 	{
 		userRoute.POST("/login", userLogin)
-		userRoute.POST("/register", userRegister)
+		userRoute.POST("/register", userRegister, middlewares.AuthMiddleware())
 	}
 }
