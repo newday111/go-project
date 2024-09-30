1e0875fed6bd36c9698afcd949b928190abc6c13
gengzeshi <gengzeshi@51jzcloud.com>
2024-09-30 22:52:53 +0800

    delete:删除注册路由权限设置

--- routes/userRoute/userRoute.go
+++ routes/userRoute/userRoute.go
@@ -2,13 +2,12 @@ package userRoute
 
 import (
 	"github.com/gin-gonic/gin"
-	"goPro4/middlewares"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
 	userRoute := Router.Group("/user")
 	{
 		userRoute.POST("/login", userLogin)
-		userRoute.POST("/register", userRegister, middlewares.AuthMiddleware())
+		userRoute.POST("/register", userRegister)
 	}
 }
