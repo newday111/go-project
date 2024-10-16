0aa27e23dd66c39824573a8395f3b66741a6d4c1
gengzeshi <gengzeshi@51jzcloud.com>
2024-10-16 16:32:10 +0800

    fix:删除代码

--- routes/userRoute/userModel.go
+++ routes/userRoute/userModel.go
@@ -7,13 +7,12 @@ import (
 const loginUserTableName = "login_users"
 
 type loginUser struct {
-	UserName string `gorm:"username"`
-	Token    string `gorm:"token"`
-	IsValid  int    `gorm: "is_valid"`
+	Name    string `gorm:"name"`
+	Token   string `gorm:"token"`
+	IsValid int    `gorm: "is_valid"`
 }
 
-func (userInfo *loginUser) getUserInfoToName(username string) *loginUser {
+func (userInfo *loginUser) getUserInfoToName(username string) {
 	//	指定查询的表名
 	db.DB.Table(loginUserTableName).Select([]string{"name", "token", "is_valid"}).Where("name = ?", username).Find(userInfo)
-	return userInfo
 }
