066b4ec3985b8d583c2ed612fe3d4fb263b4e83d
gengzeshi <gengzeshi@51jzcloud.com>
2024-10-16 16:38:35 +0800

    fix: delete

--- routes/albumRoute/albumHandler.go
+++ routes/albumRoute/albumHandler.go
@@ -3,15 +3,14 @@ package albumRoute
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"goPro4/utils"
 )
 
 func albumQuery(c *gin.Context) {
 	fmt.Println("albumQuery")
-	utils.Response(c, utils.RespGetDataSuccess, utils.RespMsg[utils.RespGetDataSuccess], nil)
+	//utils.Response(c, utils.RespGetDataSuccess, utils.RespMsg[utils.RespGetDataSuccess], nil)
 }
 
 func albumUpdate(c *gin.Context) {
 	fmt.Println("albumQuery")
-	utils.Response(c, utils.RespGetDataSuccess, utils.RespMsg[utils.RespGetDataSuccess], nil)
+	//utils.Response(c, utils.RespGetDataSuccess, utils.RespMsg[utils.RespGetDataSuccess], nil)
 }
