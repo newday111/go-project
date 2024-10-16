52ea7ed8b83feea3f5032af8ec12f83e236eefa4
gengzeshi <gengzeshi@51jzcloud.com>
2024-10-16 16:32:45 +0800

    fix: 修改代码

--- routes/userRoute/userHandler.go
+++ routes/userRoute/userHandler.go
@@ -24,42 +24,53 @@ func getUserStruct(c *gin.Context) (*User, error) {
 }
 
 func userLogin(c *gin.Context) {
+
+	//	get the params passed in the front-end
+
 	user, err := getUserStruct(c)
 	if err != nil {
-		utils.Response(c, utils.RespParamsError, utils.RespMsg[utils.RespParamsError], nil)
+		utils.Error(utils.UserRespMsg[utils.UserParamsAnalysisFailed])
+		utils.Response(c, utils.RecordLogGetFail, utils.UserParamsAnalysisFailed, utils.UserRespMsg[utils.UserParamsAnalysisFailed], nil)
 		return
 	}
 
-	info := &loginUser{
-		UserName: user.UserName,
+	//	query user information and verify it
+
+	userInfo := &loginUser{}
+	userInfo.getUserInfoToName(user.UserName)
+
+	if userInfo.Name == "" {
+		utils.Error(utils.UserRespMsg[utils.UserQueryFailed] + user.UserName)
+		utils.Response(c, utils.RecordLogGetFail, utils.UserQueryFailed, utils.UserRespMsg[utils.UserQueryFailed], nil)
+		return
 	}
 
-	userInfo := info.getUserInfoToName(user.UserName)
+	// generate tokens with user information
 
-	//	生成token
 	jwtKey := []byte(viper.GetString("login_auth.secret_key"))
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": userInfo.UserName,
+		"username": userInfo.Name,
 		"exp":      time.Now().Add(time.Hour * 24).Unix(), // 设置过期时间为24小时后
 	})
 
-	//	返回字符串token,并且校验是否生成token成功
+	//	convert the generate token into a string
+
 	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
-		utils.Response(c, utils.RespCreateTokenFail, utils.RespMsg[utils.RespCreateTokenFail], nil)
+		utils.Error(utils.UserRespMsg[utils.UserTokenCreateFailed] + "=====>" + user.UserName)
+		utils.Response(c, utils.RecordLogCreateFail, utils.UserTokenCreateFailed, utils.UserRespMsg[utils.UserTokenCreateFailed], nil)
 		return
 	}
 
 	c.Header("Authorization", tokenString)
-	c.Set("username", userInfo.UserName)
+	c.Set("username", userInfo.Name)
 
 	cookPath := viper.GetString("cookies.cook_path")
 	cookIp := viper.GetString("cookies.cook_ip")
 	c.SetCookie("username", "小明", 3600, cookPath, cookIp, false, false)
-
-	utils.Response(c, utils.RespSuccess, utils.RespMsg[utils.RespSuccess], nil)
+	utils.Response(c, utils.RecordLogSuccess, utils.UserLoginSuccess, utils.UserRespMsg[utils.UserLoginSuccess], nil)
 }
 
 func userRegister(c *gin.Context) {
-	utils.Response(c, 200, "success", nil)
+	//utils.Response(c,200, "success", nil)
 }
