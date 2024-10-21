c6873ac4d10dfe2926f2699e488e918f5b12fb1d
gengzeshi <gengzeshi@51jzcloud.com>
2024-10-21 14:05:44 +0800

    add: 新状态

--- utils/reponseStatus.go
+++ utils/reponseStatus.go
@@ -18,6 +18,8 @@ const (
 	UserTokenCreateFailed    = 402
 	UserParamsAnalysisFailed = 403
 	UserTokenExpire          = 405
+	UserCreateFailed         = 406
+	UserRegisterFailed       = 407
 	//RespGetDataSuccess  = 201
 	//RespParamsError     = 400
 	//RespAuthFail        = 401
@@ -32,7 +34,17 @@ var UserRespMsg = map[int]string{
 	402: "用户创建方式失败,请重新登录",
 	403: "解析前端参数错误",
 	405: "请重新登录",
+	406: "注册用户失败, 用户名或邮箱已经注册",
+	407: "用户注册失败",
 	//401: "没有登录,无权限访问该信息",
 	//402: "登录token生成失败",
 	//500: "服务器错误",
+}
+
+const (
+	VerificationCodeSendSuccess = 400
+)
+
+var VerificationCodeRespMsg = map[int]string{
+	400: "验证码发送成功",
 }
