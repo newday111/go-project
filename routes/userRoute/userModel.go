c4bd43f164ac51c784ead46083d96d1372620026
gengzeshi <gengzeshi@51jzcloud.com>
2024-10-21 14:07:26 +0800

    add: 注册

--- routes/userRoute/userModel.go
+++ routes/userRoute/userModel.go
@@ -4,7 +4,7 @@ import (
 	"goPro4/db"
 )
 
-const loginUserTableName = "login_users"
+const loginUserTableName = "user_info"
 
 type loginUser struct {
 	Name    string `gorm:"name"`
@@ -12,7 +12,17 @@ type loginUser struct {
 	IsValid int    `gorm: "is_valid"`
 }
 
-func (userInfo *loginUser) getUserInfoToName(username string) {
+func (userInfo *loginUser) getUserInfoToName(username, email string) {
 	//	指定查询的表名
-	db.DB.Table(loginUserTableName).Select([]string{"name", "token", "is_valid"}).Where("name = ?", username).Find(userInfo)
+	db.DB.Table(loginUserTableName).Select([]string{"name", "email", "is_valid"}).Where("name = ?", username).Or("email = ?", email).Find(userInfo)
+}
+
+func (userInfo *loginUser) registerUser(user *User) (int64, error) {
+	//	指定字段创建用户
+	createResult := db.DB.Table(loginUserTableName).Select("name", "pass_word", "email").Create(user)
+	if createResult.Error != nil {
+		return 0, createResult.Error
+	}
+	return createResult.RowsAffected, nil
+
 }
