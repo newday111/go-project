a7cedc6324db7b62693cae035d689b124d79405b
gengzeshi <gengzeshi@51jzcloud.com>
2024-10-21 14:02:19 +0800

    add: redis测试

--- example/mysql_conn_test.go
+++ example/mysql_conn_test.go
@@ -7,4 +7,8 @@ import (
 
 func TestGetMysqlConn(t *testing.T) {
 	db.GetMysqlConnection()
+	//	测试的时候这样启动,正式环境需要再服务器的配置文件中配置用户和密码
+	// redis-server --protected-mode no
+
+	db.GetRedisConnection()
 }
